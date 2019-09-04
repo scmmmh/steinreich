8071c580f166453050b646cefd5e18b1fb6c1b17
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-09-04 21:43:11 +0100

    Added build scripts

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# An optional script to be run before building
+if [ -f 'pre-build' ]
+then
+    ./pre-build
+fi
+
+git pull
+export PIPENV_VENV_IN_PROJECT=True
+pipenv install
+yarn install
+node_modules/.bin/gulp
+pipenv run pelican -s publishconf.py -o output -d content
+
+# An optional script to be run after building
+if [ -f 'post-build' ]
+then
+    ./post-build
+fi
