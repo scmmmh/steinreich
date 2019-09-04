9847cb9f2fc578751a6acdef4a5418a9bf266b11
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-09-04 21:36:48 +0100

    First steps

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    pump = require('pump');
+
+gulp.task('css', function(cb) {
+    pump([
+        gulp.src('theme/src/theme.scss'),
+        sass({
+            includePaths: [
+                'node_modules/foundation-sites/scss',
+                'node_modules/motion-ui/src'
+            ]
+        }),
+        autoprefixer({
+            cascade: false
+        }),
+        gulp.dest('theme/static/css')
+    ], cb);
+});
+
+gulp.task('js', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/what-input/dist/what-input.min.js',
+            'node_modules/foundation-sites/dist/js/foundation.min.js'
+        ]),
+        concat('scripts.js'),
+        gulp.dest('theme/static/js')
+    ], cb);
+});
+
+gulp.task('default', gulp.series('css', 'js'));
+
+gulp.task('watch', gulp.series('default', function(cb) {
+    gulp.watch('theme/src/**/*.scss', gulp.series('css'));
+    cb();
+}))
