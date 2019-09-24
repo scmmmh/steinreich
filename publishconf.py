36e4afae1a81fb9a88ed696d17b0e5619dd42371
Mark Hall <mark.hall@informatik.uni-halle.de>
2019-09-24 17:39:25 +0200

    Fixed the publishconf site URL

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-#SITEURL = 'https://steinreich.uzi.uni-halle.de'
-SITEURL = 'https://steinreich.room3b.eu'
+SITEURL = 'https://steinreich.uzi.uni-halle.de'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
