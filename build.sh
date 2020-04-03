2bf49341dd31562fad2baa37d0d4a5b8e93793a4
Mark Hall <mark.hall@work.room3b.eu>
2020-04-03 09:21:58 +0100

    Source rather than execute pre-/post scripts

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # An optional script to be run before building
 if [ -f 'pre-build' ]
 then
-    ./pre-build
+    source pre-build
 fi
 
 git pull
@@ -16,5 +16,5 @@ pipenv run pelican -s publishconf.py -o output -d content
 # An optional script to be run after building
 if [ -f 'post-build' ]
 then
-    ./post-build
+    source post-build
 fi
