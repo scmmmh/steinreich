19462f311bd704381a44b9c8e7214b93a8256a49
Mark Hall <mark.hall@work.room3b.eu>
2020-04-09 10:08:27 +0100

    Switch to using poetry instead of pipenv

--- build.sh
+++ build.sh
@@ -9,9 +9,8 @@ fi
 git pull
 yarn install --frozen-lockfile --check-files --non-interactive
 node_modules/.bin/gulp
-export PIPENV_VENV_IN_PROJECT=True
-pipenv install
-pipenv run pelican -s publishconf.py -o output -d content
+poetry install --no-dev
+poetry run pelican -s publishconf.py -o output -d content
 
 # An optional script to be run after building
 if [ -f 'post-build' ]
